093a81e25802eb5d3f379d4a21bdda74c3056a04
Keval Krishna <keval.krishna@oracle.com>
2025-02-14 15:41:53 +0530

    added the login

--- src/main/java/com/example/SCM/Project/Config/SecurityConfig.java
+++ src/main/java/com/example/SCM/Project/Config/SecurityConfig.java
@@ -1,23 +1,55 @@
 package com.example.SCM.Project.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.SCM.Project.Services.Implementation.SecurityCustomUserDetailService;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityCustomUserDetailService userDetailService;
+
     @Bean
-    public UserDetailsService userDetailsService(){
-       UserDetails  user1=  User
-            .withDefaultPasswordEncoder()
-            .username("Keval")
-            .password("1234")
-            .roles("ADMIN", "USER")
-            .build();
-            var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1);
-            return inMemoryUserDetailsManager;
-            }
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        // user detail service ka object:
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
+        // password encoder ka object
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // configuration
+
+        // urls configure kiay hai ki koun se public rangenge aur koun se private
+        // rangenge
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            // authorize.requestMatchers("/home", "/register", "/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+        httpSecurity.formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
---
+++ src/main/java/com/example/SCM/Project/Services/Implementation/SecurityCustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.example.SCM.Project.Services.Implementation;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.SCM.Project.Repositories.UserRepo;
+
+
+
+@Service
+public class SecurityCustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // apne user ko load karana hai
+        return userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + username));
+
+    }
+
+}
